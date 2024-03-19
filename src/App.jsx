fb3321a384f5342ce4335818e4029ad41849cc7c
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-03-19 22:55:59 +0100

    return power

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import './App.css';
 import { useState } from 'react';
 
 function App () {
+  const [nombre, setNombre] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`nombre:${nombre}, imagen:${imagen}`);
+  };
+
   return (
 <>
-  <h1>Hola</h1>
+  <h1>Poke form</h1>
+  <form onSubmit={handleSubmit} >
+        <label htmlFor="Nombre">PokeNombre </label>
+        <input
+          type="text"
+          id="nombre"
+          name="nombre"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
   </>
   )
   
